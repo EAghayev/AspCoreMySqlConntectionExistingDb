8a61a54875ee786c6a4c665bf2fe8409e98f405e
Elchin Aghayev <eaghayevv@gmail.com>
2020-06-10 11:10:25 +0400

    complete CopyFilesToAnotherDirectory method

--- TechlandMysqlMigrateApi/Controllers/PostController.cs
+++ TechlandMysqlMigrateApi/Controllers/PostController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
@@ -24,6 +25,41 @@ namespace TechlandMysqlMigrateApi.Controllers
         {
             return Ok(_context.WpPosts.OrderByDescending(d=>d.PostDate).Take(10).ToList());
         }
-    }
 
+
+        [HttpGet("changefiledirectory")]
+        public async Task<IActionResult> CopyFilesToAnotherDirectory()
+        {
+            List<WpPosts> imagelessPosts = new List<WpPosts>();
+            var list = Directory.GetFiles(@"C:\Users\Eagha\Desktop\Files", "*", SearchOption.AllDirectories);
+            var images = _context.WpPosts.Where(p => p.PostDate>=new DateTime(2020,5,1) && p.PostType == "attachment" && p.Guid != null && !String.IsNullOrWhiteSpace(p.Guid) && (p.PostMimeType== "image/jpeg" || p.PostMimeType== "image/png" || p.PostMimeType=="image/gif")).ToList();
+            foreach (var image in images)
+            {
+                var imgArr = image.Guid.Split('/');
+                var imageName =@"\"+ imgArr[imgArr.Length - 1];
+
+                string sourceFile = list.FirstOrDefault(l => l.Contains(imageName));
+                string destinationFile = @"C:\Users\Eagha\Desktop\NewFiles\News"+ imageName;
+
+                System.IO.File.Copy(sourceFile, destinationFile,true);
+
+                var newImgArr = imageName.Split('.');
+                newImgArr[newImgArr.Length - 2] = newImgArr[newImgArr.Length - 2] + "-341x220";
+                var thumbName =  String.Join('.',newImgArr);
+
+                string sourceFileThumb = list.FirstOrDefault(l => l.Contains(thumbName));
+                string destinationFileThumb = @"C:\Users\Eagha\Desktop\NewFiles\News\Thumbs"+thumbName;
+
+                if (string.IsNullOrWhiteSpace(sourceFileThumb))
+                {
+                    imagelessPosts.Add(image);
+                    continue;
+                }
+
+                System.IO.File.Copy(sourceFileThumb, destinationFileThumb,true);
+            }
+
+            return Ok(imagelessPosts);
+        }
+    }
 }
--- TechlandMysqlMigrateApi/Startup.cs
+++ TechlandMysqlMigrateApi/Startup.cs
@@ -44,6 +44,7 @@ namespace TechlandMysqlMigrateApi
             app.UseRouting();
 
             app.UseAuthorization();
+            app.UseStaticFiles();
 
             app.UseEndpoints(endpoints =>
             {
